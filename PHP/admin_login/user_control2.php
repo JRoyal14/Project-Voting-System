3d510247acd3dbe5b0c8171ec37f4c3e8c27985a
JRoyal14 <jezza_royal@hotmail.com>
2017-04-15 19:56:11 +0100

    Add files via upload

---
+++ PHP/admin_login/user_control2.php
@@ -0,0 +1,50 @@
+<?php
+include_once 'connection3.php';
+
+class User {
+	
+	private $db;
+	private $connection;
+	
+	function __construct() {
+		$this -> db = new DB_Connection();
+		$this -> connection = $this->db->getConnection();
+	}
+	
+	public function does_user_exist($username,$password)
+	{
+		$encrypted_password = md5($password);
+		$query = "Select * from admin_login where username='$username' and password = '$encrypted_password' ";
+		$result = mysqli_query($this->connection, $query);
+		if(mysqli_num_rows($result)>0){
+			$json['success'] = ' Welcome '.$username;
+			echo json_encode($json);
+			mysqli_close($this -> connection);
+			}else{
+				$json['error'] = 'Wrong password';
+			}
+			echo json_encode($json);
+			mysqli_close($this->connection);
+		}
+		
+	}
+	
+
+
+
+$user = new User();
+if(isset($_POST['username'],$_POST['password'])) {
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	
+	if(!empty($username) && !empty($password)){
+		
+		$encrypted_password = md5($password);
+		$user-> does_user_exist($username,$password);
+		
+	}else{
+		echo json_encode("you must type both inputs");
+	}
+	
+}
+?>
